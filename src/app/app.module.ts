84309f624fc4de9b1725ee2b4079d41977f2f26a
Edson Iván Badillo Ramírez <EdsonBadillo@High-flyerss-MacBook-Pro-2.local>
2019-08-16 17:24:49 -0500

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './registro/registro.component';
+import {MatButtonModule} from '@angular/material';
+ 
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { RegistroComponent } from './registro/registro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
